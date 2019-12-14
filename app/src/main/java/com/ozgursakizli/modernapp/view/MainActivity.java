f6c07c47bd80f3c088fb1628522462d9e53f5c86
TCOSAKIZLI <ozgur.sakizli@turkcell.com.tr>
2019-12-14 12:26:05 +0300

    activity moved under view package

--- app/src/main/java/com/ozgursakizli/modernapp/view/MainActivity.java
+++ app/src/main/java/com/ozgursakizli/modernapp/view/MainActivity.java
@@ -1,8 +1,10 @@
-package com.ozgursakizli.modernapp;
+package com.ozgursakizli.modernapp.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.ozgursakizli.modernapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
